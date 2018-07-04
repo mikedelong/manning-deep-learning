0c27cf2cffe7df3938a05515f7aa645db431eecd
Mike DeLong <mikedelong@outlook.com>
2018-07-04 18:42:26 -0400

    feature: logging and timing feature: load IMDB data

---
+++ code/page068.py
@@ -0,0 +1,38 @@
+import logging
+import time
+from os.path import isdir
+
+from keras.datasets import imdb
+
+if __name__ == '__main__':
+    start_time = time.time()
+
+    formatter = logging.Formatter('%(asctime)s : %(name)s :: %(levelname)s : %(message)s')
+    logger = logging.getLogger('main')
+    logger.setLevel(logging.DEBUG)
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+    console_handler.setLevel(logging.DEBUG)
+    logger.debug('started')
+
+    verbose = 0
+
+    output_folder = '../output/'
+
+    output_folder_exists = isdir(output_folder)
+    if not output_folder_exists:
+        logger.warning('output folder %s does not exist. Quitting.' % output_folder)
+        quit()
+
+    num_words = 10000
+    logger.debug('loading IMDB data, top %d most frequent words' % (num_words))
+    (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=num_words)
+
+    logger.debug('done')
+    finish_time = time.time()
+    elapsed_hours, elapsed_remainder = divmod(finish_time - start_time, 3600)
+    elapsed_minutes, elapsed_seconds = divmod(elapsed_remainder, 60)
+    logger.info("Time: {:0>2}:{:0>2}:{:05.2f}".format(int(elapsed_hours), int(elapsed_minutes), elapsed_seconds))
+    console_handler.close()
+    logger.removeHandler(console_handler)
