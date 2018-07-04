83a2bb69adaac38d435f4775f66e11422522a4de
Mike DeLong <mikedelong@outlook.com>
2018-07-04 18:45:54 -0400

    cleanup: narrower time import cleanup: remove redundant parentheses

--- code/page068.py
+++ code/page068.py
@@ -1,11 +1,11 @@
 import logging
-import time
 from os.path import isdir
+from time import time
 
 from keras.datasets import imdb
 
 if __name__ == '__main__':
-    start_time = time.time()
+    start_time = time()
 
     formatter = logging.Formatter('%(asctime)s : %(name)s :: %(levelname)s : %(message)s')
     logger = logging.getLogger('main')
@@ -17,7 +17,6 @@ if __name__ == '__main__':
     logger.debug('started')
 
     verbose = 0
-
     output_folder = '../output/'
 
     output_folder_exists = isdir(output_folder)
@@ -26,11 +25,11 @@ if __name__ == '__main__':
         quit()
 
     num_words = 10000
-    logger.debug('loading IMDB data, top %d most frequent words' % (num_words))
+    logger.debug('loading IMDB data, top %d most frequent words' % num_words)
     (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=num_words)
 
     logger.debug('done')
-    finish_time = time.time()
+    finish_time = time()
     elapsed_hours, elapsed_remainder = divmod(finish_time - start_time, 3600)
     elapsed_minutes, elapsed_seconds = divmod(elapsed_remainder, 60)
     logger.info("Time: {:0>2}:{:0>2}:{:05.2f}".format(int(elapsed_hours), int(elapsed_minutes), elapsed_seconds))
